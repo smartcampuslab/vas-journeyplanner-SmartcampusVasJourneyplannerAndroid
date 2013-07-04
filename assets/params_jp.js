5bff684a4d1150e22694d1c27bb3b4f12a4a262b
chin8 <m.chini@trentorise.eu>
2013-07-04 18:26:51 +0200

    New maps New timetable files

---
+++ assets/params_jp.js
@@ -0,0 +1,47 @@
+/*
+ * SMART CHECK OPTIONS
+ * 1 bus trento timetable
+ * 2 bus rovereto timetable
+ * 3 suburban timetable
+ * 4 train timetable
+ * 5 parking trento
+ * 6 parking rovereto
+ * 
+ * BROADCAST NOTIFICATIONS OPTIONS
+ * 1 bus trento delay
+ * 2 bus rovereto delay
+ * 3 bus suburban delay
+ * 4 train delay
+ * 5 accident
+ * 6 road works
+ * 7 strike
+ * 8 traffic jam
+ * 9 diversion
+ * 
+ * COORDINATES
+ * Trento 46.069672, 11.121270
+ * Rovereto 45.890919, 11.040184
+ */
+
+{
+    "app_token" : "viaggiarovereto",
+    "smartcheck_options" : [
+        1,
+        4,
+        5
+    ],
+    "broadcast_notifications_options" : [
+        1,
+        4,
+        5,
+        6,
+        7,
+        8,
+        9
+    ],
+   "center_map":[
+      46.069672,
+      11.121270
+   ],
+   "zoom_map":15
+}
