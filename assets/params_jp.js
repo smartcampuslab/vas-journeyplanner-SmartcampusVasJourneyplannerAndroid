0be42e6b61473a2147f49d6b60d5d4b7dc01c327
raman <raman@fbk.eu>
2013-12-17 16:52:51 +0100

    tutorial and parameters aligned

--- assets/params_jp.js
+++ assets/params_jp.js
@@ -1,4 +1,8 @@
 /*
+ * SHOW ALERTROADS IN LEG MAP (planning)
+ * "alertroads_in_planning" : true,
+ * "alertroads_in_planning_agencyid" : "COMUNE_DI_ROVERETO",
+ * 
  * SMART CHECK OPTIONS
  * 1 bus trento timetable
  * 2 bus rovereto timetable
@@ -6,6 +10,15 @@
  * 4 train timetable
  * 5 parking trento
  * 6 parking rovereto
+ * 7 alerts rovereto
+ * 
+ * SUBURBAN ZONES
+ * 1 Val di Fiemme, Val di Fassa, Val di Cembra
+ * 2 Val Rendena, Giudicarie
+ * 3 Rovereto, Lavarone, Destra Adige, Riva del Garda
+ * 4 Valsugana
+ * 5 Valle di Primiero
+ * 6 Val di Non, Val di Sole
  * 
  * BROADCAST NOTIFICATIONS OPTIONS
  * 1 bus trento delay
@@ -21,6 +34,10 @@
  * COORDINATES
  * Trento 46.069672, 11.121270
  * Rovereto 45.890919, 11.040184
+ * 
+ *     "suburban_zones" : [
+ *        3
+ *   ],
  */
 
 {
